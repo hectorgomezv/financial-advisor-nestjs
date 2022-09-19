b965bd4708796948bcd743d4f51312627c97e795
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-09-19 19:09:25 +0200

    Fix tests

--- src/metrics/routes/metrics.controller.spec.ts
+++ src/metrics/routes/metrics.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MetricsService } from '../domain/metrics.service';
 import { MetricsController } from './metrics.controller';
 
 describe('MetricsController', () => {
@@ -7,6 +8,7 @@ describe('MetricsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MetricsController],
+      providers: [MetricsService],
     }).compile();
 
     controller = module.get<MetricsController>(MetricsController);
