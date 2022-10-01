65beae847384a75c5008ff8c73712c2cd3a49e4d
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-10-01 17:31:38 +0200

    Update docker-compose

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const { FINANCIAL_ADVISOR_NESTJS_HTTP_SERVER_PORT, HTTP_SERVER_PORT } =
-  process.env;
+const { HTTP_SERVER_PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
-  await app.listen(
-    Number(FINANCIAL_ADVISOR_NESTJS_HTTP_SERVER_PORT) ||
-      Number(HTTP_SERVER_PORT) ||
-      3000,
-  );
+  await app.listen(Number(HTTP_SERVER_PORT) || 3000);
 }
 bootstrap();
