7d4ab0c700fd5e11fc8c82071e72d2b378008788
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-10-01 16:34:14 +0200

    Externalize MongoDB connection config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
@@ -16,7 +16,15 @@ import { PortfoliosModule } from './portfolios/portfolios.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb://localhost:27017/financialAdvisorDB'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: `${config.get<string>(
+          'MONGO_CONNECTION_STRING',
+        )}/${config.get<string>('MONGO_DATABASE_NAME')}`,
+      }),
+    }),
     ScheduleModule.forRoot(),
     CompaniesModule,
     HealthModule,
