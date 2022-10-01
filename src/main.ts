fbd0da0f69826231198628762261e99de69e6712
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-10-01 17:28:16 +0200

    Configure different http ports by environment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { FINANCIAL_ADVISOR_NESTJS_HTTP_SERVER_PORT, HTTP_SERVER_PORT } =
+  process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
-  await app.listen(3000);
+  await app.listen(
+    Number(FINANCIAL_ADVISOR_NESTJS_HTTP_SERVER_PORT) ||
+      Number(HTTP_SERVER_PORT) ||
+      3000,
+  );
 }
 bootstrap();
