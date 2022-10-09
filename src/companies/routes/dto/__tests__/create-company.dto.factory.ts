137852b644c806b5e94b35e000fece5a5c8a53e9
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-10-09 12:03:13 +0200

    Add companies e2e tests

---
+++ src/companies/routes/dto/__tests__/create-company.dto.factory.ts
@@ -0,0 +1,12 @@
+import { faker } from '@faker-js/faker';
+import { CreateCompanyDto } from '../create-company.dto';
+
+export function createCompanyDtoFactory(
+  name?: string,
+  symbol?: string,
+): CreateCompanyDto {
+  return <CreateCompanyDto>{
+    name: name ?? faker.random.words(),
+    symbol: symbol ?? 'CRM',
+  };
+}
