b5df5129d4dd1a3b64a6fee114f1a1af548ae32c
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-11-11 17:35:55 +0100

    Add nginx, mongo backup script

---
+++ docker/mongo/scripts/backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Usage
+## DB=financialAdvisorDB ADMIN_PASSWORD=myAdminPassword sh backup.sh
+
+set -e
+BACKUP_NAME=$DB-$(date +%y%m%d_%H%M%S).gz
+
+date
+
+echo "Dumping MongoDB $DB database to compressed archive"
+docker exec financial-advisor-nestjs-fa-mongo-1 mongodump --db $DB --archive=$HOME/$BACKUP_NAME --gzip -u admin --authenticationDatabase=admin -p $ADMIN_PASSWORD 
+docker cp financial-advisor-nestjs-fa-mongo-1:/$HOME/$BACKUP_NAME /root/backup/
+
