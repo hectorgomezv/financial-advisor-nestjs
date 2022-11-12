2fceedd5f8236e5a84027283f31187e4ab4081b1
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-11-12 20:14:03 +0100

    Fix backup script

--- docker/mongo/scripts/backup.sh
+++ docker/mongo/scripts/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Usage
-## DB=financialAdvisorDB ADMIN_USERNAME=myAdminUser ADMIN_PASSWORD=myAdminPassword sh backup.sh
+## DB=financialAdvisorDB DB_USER=myAdminUser DB_PASSWORD=myAdminPassword sh backup.sh
 
 set -e
 BACKUP_NAME=$DB-$(date +%y%m%d_%H%M%S).gz
@@ -9,6 +9,6 @@ BACKUP_NAME=$DB-$(date +%y%m%d_%H%M%S).gz
 date
 
 echo "Dumping MongoDB $DB database to compressed archive"
-docker exec financial-advisor-nestjs-fa-mongo-1 mongodump --db $DB --archive=$HOME/$BACKUP_NAME --gzip -u $ADMIN_USERNAME --authenticationDatabase=admin -p $ADMIN_PASSWORD 
+docker exec financial-advisor-nestjs-fa-mongo-1 mongodump --db $DB --archive=$HOME/$BACKUP_NAME --gzip -u $DB_USER --authenticationDatabase=$DB -p $DB_PASSWORD 
 docker cp financial-advisor-nestjs-fa-mongo-1:/$HOME/$BACKUP_NAME /root/backup/
 
