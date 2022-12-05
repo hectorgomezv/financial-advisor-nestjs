ec147aaa564bf6a8f4d3f271e1ef4ce5cea9cf41
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-12-05 23:16:30 +0100

    Fix binary path

--- docker/mongo/scripts/backup-upload.sh
+++ docker/mongo/scripts/backup-upload.sh
@@ -9,4 +9,4 @@ BACKUP_PREFIX=$DB-$(date +%y%m%d)
 date
 
 echo "Uploading backup for $DB database"
-dbxcli put /root/backup/$BACKUP_PREFIX*.gz backups/financial-advisor/$DB/$BACKUP_PREFIX.gz
+/usr/local/bin/dbxcli put /root/backup/$BACKUP_PREFIX*.gz backups/financial-advisor/$DB/$BACKUP_PREFIX.gz
